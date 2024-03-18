87c18a7659d9264844779e0057bc5df188a3d548
cripiii25 <juanlondonoierml@gmail.com>
2024-03-18 16:17:53 -0500

    Additional sections (Hero, Experience, Projects), other components, and icons have been added.

---
+++ personInfo.ts
@@ -0,0 +1,6 @@
+export const personInfo = {
+  name: 'Juan Manuel', 
+  linkedin: 'linkedin.com/in/juan-manuel-londoño-gonzález-76759829b/',
+  github: 'https://github.com/cripiii25',
+  email: 'mailto:juanlondonoierml@gmail.com'
+}
