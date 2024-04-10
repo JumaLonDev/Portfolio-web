670c9818498ad92e6564053b959fdc1c75937602
JumaLonDev <jumalomdev@gmail.com>
2024-04-09 20:14:12 -0500

    The email, repository name, and project image have been changed.

--- personInfo.ts
+++ personInfo.ts
@@ -1,6 +1,6 @@
 export const personInfo = {
   name: 'Juan Manuel', 
   linkedin: 'https://linkedin.com/in/juan-manuel-londoño-gonzález-76759829b/',
-  github: 'https://github.com/cripiii25',
-  email: 'mailto:juanlondonoierml@gmail.com'
+  github: 'https://github.com/JumaLonDev',
+  email: 'mailto:jumalomdev@gmail.com'
 }
