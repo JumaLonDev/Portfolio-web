63aed516ab20464c70910e048a10dac264f9ef6c
JumaLonDev <jumalomdev@gmail.com>
2024-09-23 10:42:02 -0500

    The button to open the CV has been added.

--- personInfo.ts
+++ personInfo.ts
@@ -2,5 +2,6 @@ export const personInfo = {
   name: 'Juan Manuel', 
   linkedin: 'https://www.linkedin.com/in/jumalondev/',
   github: 'https://github.com/JumaLonDev',
-  email: 'mailto:jumalomdev@gmail.com'
+  email: 'mailto:jumalomdev@gmail.com',
+  cv: 'https://drive.google.com/file/d/1XLMFXCJxRQGEnKLqlkqq9VbGc6lOVN_x/view?usp=sharing'
 }
