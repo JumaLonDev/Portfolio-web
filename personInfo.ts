e42a9c2cc2c31b7ce5ca29dfeb753213366f80a8
Juan Manuel <88118695+JumaLonDev@users.noreply.github.com>
2025-08-03 15:31:36 -0500

    Update personInfo.ts

--- personInfo.ts
+++ personInfo.ts
@@ -3,5 +3,5 @@ export const personInfo = {
   linkedin: 'https://www.linkedin.com/in/jumalondev/',
   github: 'https://github.com/JumaLonDev',
   email: 'mailto:jumalomdev@gmail.com',
-  cv: 'https://drive.google.com/file/d/1XLMFXCJxRQGEnKLqlkqq9VbGc6lOVN_x/view?usp=sharing'
+  cv: 'https://drive.google.com/file/d/1QnID_PRG-n2ZBaKsjB83u4Pjy7FO2Swr/view?usp=sharing'
 }
